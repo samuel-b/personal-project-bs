64b692418ab4feeb16bf965714696051a6739c8a
samuel-b <94946872+samuel-b@users.noreply.github.com>
2022-03-11 15:25:34 +1100

    updated nav and home

---
+++ index.js
@@ -0,0 +1,20 @@
+// const homePage= document.querySelectorAll(".nav-link")[0];
+// const aboutPage= document.querySelectorAll(".nav-link")[1];
+// const projectsPage= document.querySelectorAll(".nav-link")[3];
+// const contactPage= document.querySelectorAll(".nav-link")[4];
+// // const currentPage = window.location.hash; 
+
+
+// switch (currentPage) {
+//   case "#about":
+//     console.log("about");
+//     // aboutPage.classList.add("active")
+//     break;
+//     case "#projects":
+//       console.log("projects");
+//       // aboutPage.classList.add("active")
+//       break;
+
+//   default:
+//     break;
+// }
