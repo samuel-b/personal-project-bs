57e94a733bedb087d51098066bf8b5fcf98c4e08
Sam-B-Hub <64763981+Sam-B-Hub@users.noreply.github.com>
2022-03-23 20:25:17 +1100

    recommended modifications

--- index.js
+++ index.js
@@ -2,3 +2,13 @@
 const currentYear = new Date
 //Uses a method to get the current year from the date object and updates the footer
 document.querySelector(".footer-year").innerHTML = "© " + currentYear.getFullYear() + " Samuel Brooker"
+
+//Contact Page - Email click to copy
+
+const contactEmail = document.querySelector("#contactEmail");
+
+contactEmail.addEventListener("click", () => {
+  const emailAddress = "email@email.com"
+  navigator.clipboard.writeText(emailAddress)
+  contactEmail.firstChild.data = "Email (Copied)"
+})
